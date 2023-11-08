848aadf64dc654d67d7fa30d77b2589334ff260c
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-11-08 09:38:57 -0500

    Update Figura.java

--- SEGUNDOCORTE/ClasesAbstractas/abbsrt/Figura.java
+++ SEGUNDOCORTE/ClasesAbstractas/abbsrt/Figura.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package abbstr;
 
-/**
- *
- * @author Estudiantes
- */
-public class Figura {
-    
+public abstract class Figura {
+	private String color;
+
+	public Figura(String color) {
+		this.color = color;
+	}
+	
+	public abstract double calcularArea();
+
+	public String getColor() {
+		return color;
+	}		
 }
