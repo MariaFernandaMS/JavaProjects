0410855970e92a039c0822e31286513d4ff05e76
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-11-08 09:39:24 -0500

    Update Cuadrado.java

--- SEGUNDOCORTE/ClasesAbstractas/abbsrt/Cuadrado.java
+++ SEGUNDOCORTE/ClasesAbstractas/abbsrt/Cuadrado.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package abbstr;
 
-/**
- *
- * @author Estudiantes
- */
-public class Cuadrado {
-    
+public class Cuadrado extends Figura {
+	private double lado;
+
+	public Cuadrado(String color, double lado) {
+		super(color);
+		this.lado = lado;
+	}
+
+
+	@Override
+	public double calcularArea() {
+		return lado*lado;
+	}	
 }
