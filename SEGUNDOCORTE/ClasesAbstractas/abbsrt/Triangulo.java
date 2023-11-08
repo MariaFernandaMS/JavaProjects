54ac11338701ef4b940a4aaad0ee1aa197adc18c
MariaFernandaMS <142989111+MariaFernandaMS@users.noreply.github.com>
2023-11-08 09:38:15 -0500

    Update Triangulo.java

--- SEGUNDOCORTE/ClasesAbstractas/abbsrt/Triangulo.java
+++ SEGUNDOCORTE/ClasesAbstractas/abbsrt/Triangulo.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package abbstr;
 
-/**
- *
- * @author Estudiantes
- */
-public class Triangulo {
-    
+public class Triangulo extends Figura
+{
+    private double base;
+    private double altura;
+
+    public Triangulo(String color, double base, double altura)
+    {
+        super(color);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea()
+    {
+        return (base * altura) / 2;
+    }
 }
